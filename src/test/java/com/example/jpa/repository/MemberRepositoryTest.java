24c0a525bf4f3fba3706b92da8074c7f6fb3b058
LeeJunGyun <rainmaker0303@hanmail.net>
2021-04-29 21:40:16 +0900

    save, saveAll performance test

---
+++ src/test/java/com/example/jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.example.jpa.repository;
+
+import com.example.jpa.domain.Member;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * RunWith 는 테스트를 실행시켜주는 것이고. SpringRunner 는 빈들을 올린다. SpringBootTest 와는 다르게 다 올리지는 않음.
+ * DataJpaTest 는 인메모리 데이터베이스로 설정하고 엔티티 스캔.
+ * 2개 Annotation 은 같이 써야 함. 스프링 공식문서에 나와있음.
+ * reference : https://4whomtbts.tistory.com/128
+ */
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private final int SIZE = 100;
+
+    @Test
+    public void save_대량_성능_테스트() throws Exception {
+        long start = System.currentTimeMillis();
+        for (int i = 0 ; i < SIZE ; i++) {
+            Member member = Member.builder()
+                    .age(10)
+                    .name("test")
+                    .telNo("123")
+                    .build();
+            memberRepository.save(member);
+        }
+        System.out.println("elapsed time : "  + (System.currentTimeMillis() - start));
+    }
+
+    @Test
+    public void saveAll_대량_성능_테스트() throws Exception {
+        long start = System.currentTimeMillis();
+        List<Member> members = new ArrayList<>();
+        for (int i = 0 ; i < SIZE ; i++) {
+            Member member = Member.builder()
+                    .age(10)
+                    .name("test")
+                    .telNo("123")
+                    .build();
+            members.add(member);
+        }
+        memberRepository.saveAll(members);
+        System.out.println("elapsed time : "  + (System.currentTimeMillis() - start));
+    }
+}
