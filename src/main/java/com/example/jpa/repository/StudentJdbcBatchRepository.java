f16da74efe2c5313935cf07792f0a95ff25ee0d3
LeeJunGyun <rainmaker0303@hanmail.net>
2021-05-28 21:17:11 +0900

    test update

---
+++ src/main/java/com/example/jpa/repository/StudentJdbcBatchRepository.java
@@ -0,0 +1,48 @@
+package com.example.jpa.repository;
+
+import com.example.jpa.domain.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
+
+import javax.transaction.Transactional;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+@Slf4j
+@Repository
+public class StudentJdbcBatchRepository {
+
+    private final int BATCH_SIZE = 1000;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Transactional
+    public void batchInsert(List<Student> students) {
+        StopWatch timer = new StopWatch();
+        String sql = "INSERT INTO `STUDENT` (student_id, name, age, address)"
+                + " VALUES(?,?,?,?)";
+        timer.start();
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                Student student = students.get(i);
+                ps.setLong(1, student.getId());
+                ps.setString(2, student.getName());
+                ps.setInt(3, student.getAge());
+                ps.setString(4, student.getAddress());
+            }
+            @Override
+            public int getBatchSize() {
+                return BATCH_SIZE;
+            }
+        });
+        timer.stop();
+        log.info("batchInsert -> Total time in seconds: " + timer.getTotalTimeSeconds());
+    }
+}
--- src/test/java/com/example/jpa/repository/StudentJdbcBatchRepositoryTest.java
+++ src/test/java/com/example/jpa/repository/StudentJdbcBatchRepositoryTest.java
@@ -1,4 +1,38 @@
 package com.example.jpa.repository;
 
+import com.example.jpa.domain.Student;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class StudentJdbcBatchRepositoryTest {
+
+    @Autowired
+    private StudentJdbcBatchRepository studentJdbcBatchRepository;
+
+    @Test
+    public void STUDENT_JDBC_BATCH_TEST() {
+        List<Student> students = new ArrayList<Student>() {
+            {
+                for (int i = 0 ; i < 200000 ; i++) {
+                    add(Student.builder()
+                            .id(Long.valueOf(i) + 1)
+                            .name("test" + i)
+                            .age(20)
+                            .address("test" + i)
+                            .etc("test" + i)
+                            .build());
+                }
+            }
+        };
+
+        studentJdbcBatchRepository.batchInsert(students);
+    }
 }
